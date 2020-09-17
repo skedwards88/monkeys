de5fda93d0a3e1257fb50cb82fcb8d472538e81a
skedwards88 <skedwards88@gmail.com>
2020-09-17 10:35:20 -0700

    Updates

---
+++ precache-manifest.2271647bbd2fb4f3e8147b9a81330c3e.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a91991e331311992ac91740796df1b6a",
+    "url": "/monkeys/index.html"
+  },
+  {
+    "revision": "e0c1f185644ad27625c8",
+    "url": "/monkeys/static/css/main.df82349c.chunk.css"
+  },
+  {
+    "revision": "0fc43e6916e8bf2c37d6",
+    "url": "/monkeys/static/js/2.132a2f25.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/monkeys/static/js/2.132a2f25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c1f185644ad27625c8",
+    "url": "/monkeys/static/js/main.261fe0d2.chunk.js"
+  },
+  {
+    "revision": "d8c1ad37f0207feae652",
+    "url": "/monkeys/static/js/runtime-main.1ec2a951.js"
+  },
+  {
+    "revision": "db11c5ac7dbddf2db19c8cf85bff9a5e",
+    "url": "/monkeys/static/media/Blue_Icon.db11c5ac.png"
+  },
+  {
+    "revision": "d3d6dbc8fd747d11315297d63367928a",
+    "url": "/monkeys/static/media/Monkeys_Tile_1.d3d6dbc8.png"
+  },
+  {
+    "revision": "1623bd72ea889224c6aa3b8fa8c2d967",
+    "url": "/monkeys/static/media/Monkeys_Tile_10.1623bd72.png"
+  },
+  {
+    "revision": "67762bd0f58b1da881aa61929660e11a",
+    "url": "/monkeys/static/media/Monkeys_Tile_11.67762bd0.png"
+  },
+  {
+    "revision": "3801f95cf07b31a7e1e2abfcbed20c04",
+    "url": "/monkeys/static/media/Monkeys_Tile_12.3801f95c.png"
+  },
+  {
+    "revision": "fdbc7a8388b81bad5d8aec9ae89e357e",
+    "url": "/monkeys/static/media/Monkeys_Tile_13.fdbc7a83.png"
+  },
+  {
+    "revision": "d79a13ab5fb637a45bee4d2c83de44f5",
+    "url": "/monkeys/static/media/Monkeys_Tile_14.d79a13ab.png"
+  },
+  {
+    "revision": "7d63db080b171fc6a3d57a12c88a2cf7",
+    "url": "/monkeys/static/media/Monkeys_Tile_15.7d63db08.png"
+  },
+  {
+    "revision": "3d73272ce672b92c8d0b23a99e058fba",
+    "url": "/monkeys/static/media/Monkeys_Tile_16.3d73272c.png"
+  },
+  {
+    "revision": "38c3a0176f0ac69ac0df6ee2ee154ac8",
+    "url": "/monkeys/static/media/Monkeys_Tile_17.38c3a017.png"
+  },
+  {
+    "revision": "b5b5551c127e88efe34e95386b315833",
+    "url": "/monkeys/static/media/Monkeys_Tile_18.b5b5551c.png"
+  },
+  {
+    "revision": "4a0c1d1421cc06836ccbe15ae5720c73",
+    "url": "/monkeys/static/media/Monkeys_Tile_19.4a0c1d14.png"
+  },
+  {
+    "revision": "e43c4110e195513777ca67dcdc3f94a6",
+    "url": "/monkeys/static/media/Monkeys_Tile_2.e43c4110.png"
+  },
+  {
+    "revision": "dd5a4b55b73e8f6761d67f3e06c9eacb",
+    "url": "/monkeys/static/media/Monkeys_Tile_20.dd5a4b55.png"
+  },
+  {
+    "revision": "32a537412def2b7de5e6ccc423e708d0",
+    "url": "/monkeys/static/media/Monkeys_Tile_3.32a53741.png"
+  },
+  {
+    "revision": "46d65619091ea3ec01c7308d0eb8e83f",
+    "url": "/monkeys/static/media/Monkeys_Tile_4.46d65619.png"
+  },
+  {
+    "revision": "4a525f965ead3379acfa86b72291c320",
+    "url": "/monkeys/static/media/Monkeys_Tile_5.4a525f96.png"
+  },
+  {
+    "revision": "65b840960b835e6e6df58ce975725af7",
+    "url": "/monkeys/static/media/Monkeys_Tile_6.65b84096.png"
+  },
+  {
+    "revision": "005700340d9299d1c8c5268827e29be9",
+    "url": "/monkeys/static/media/Monkeys_Tile_7.00570034.png"
+  },
+  {
+    "revision": "71a8130ecb741f998568af9db9d11619",
+    "url": "/monkeys/static/media/Monkeys_Tile_8.71a8130e.png"
+  },
+  {
+    "revision": "c94317268e07f888e16ea70ddebfbce3",
+    "url": "/monkeys/static/media/Monkeys_Tile_9.c9431726.png"
+  },
+  {
+    "revision": "53678ea010902dfc393fb8970382425e",
+    "url": "/monkeys/static/media/Red_Icon.53678ea0.png"
+  },
+  {
+    "revision": "f1c66207cb2c66e7ca45168a3bc711a2",
+    "url": "/monkeys/static/media/board_0.f1c66207.png"
+  },
+  {
+    "revision": "e39bb7ce8b99cce316883b5a501a7841",
+    "url": "/monkeys/static/media/board_1.e39bb7ce.png"
+  },
+  {
+    "revision": "cee73207e498055b364c1b05c8fb287d",
+    "url": "/monkeys/static/media/board_2.cee73207.png"
+  },
+  {
+    "revision": "e524f6292e5a63b9bacee200dc868daa",
+    "url": "/monkeys/static/media/board_3.e524f629.png"
+  },
+  {
+    "revision": "3b453d2808f3f2822421b1ff61e44fa6",
+    "url": "/monkeys/static/media/chest.3b453d28.png"
+  },
+  {
+    "revision": "bcf7147cf3c3145ae44309733a89cddb",
+    "url": "/monkeys/static/media/coconut_count_0.bcf7147c.png"
+  },
+  {
+    "revision": "5b7ebbd15d8a2e111c2d041feadf1c81",
+    "url": "/monkeys/static/media/coconut_count_1.5b7ebbd1.png"
+  },
+  {
+    "revision": "b56000ece83fd748ef5528adbbea61d2",
+    "url": "/monkeys/static/media/coconut_count_2.b56000ec.png"
+  },
+  {
+    "revision": "4f799a637afeb51f8dbb93cbefdb806a",
+    "url": "/monkeys/static/media/coconut_count_3.4f799a63.png"
+  },
+  {
+    "revision": "1ae6fa30d4b889fe74a47fd9decef36b",
+    "url": "/monkeys/static/media/coconuts.1ae6fa30.png"
+  },
+  {
+    "revision": "481f421fb7a0075ba7dd78767f6d01c0",
+    "url": "/monkeys/static/media/monkey_3.481f421f.png"
+  },
+  {
+    "revision": "e7892eb37ac9a3139a50ff9b9f02d9a0",
+    "url": "/monkeys/static/media/route.e7892eb3.png"
+  },
+  {
+    "revision": "3eb6229db3ea76dd97f8ae671fa957ec",
+    "url": "/monkeys/static/media/route_and_anchor.3eb6229d.png"
+  },
+  {
+    "revision": "2c45f88d8b1bf50e8f7eca92fae666b4",
+    "url": "/monkeys/static/media/sea_background.2c45f88d.png"
+  },
+  {
+    "revision": "846e2372732c111eacc9f383a71d8962",
+    "url": "/monkeys/static/media/two_ships.846e2372.png"
+  }
+]);
